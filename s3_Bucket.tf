4833c3d67cf571d7dd549b61c711d83a6d06baf5
Mayurip31 <mayuripawar6196@gmail.com>
2024-02-23 21:03:01 +0530

    s3-bucket using terraform

---
+++ s3_Bucket.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+    region  = "us-east-1"
+    
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "webdata"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
