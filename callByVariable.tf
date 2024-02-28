8b53a3e2519c4a723c85ece0ef0479331e9034f5
Mayurip31 <mayuripawar6196@gmail.com>
2024-02-28 14:58:20 +0530

    call by variable

---
+++ callByVariable.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+    region  = "us-east-1"
+    
+}
+
+resource "aws_instance" "server" {
+  ami           = ami_id
+  instance_type = type
+
+  tags = {
+    Name = "Mayuri"
+  }
+
+
+
+}
+variable "ami_id" {
+    default = "ami-0c7217cdde317cfec"
+    description = "launch instnce"
+  } 
+
+variable "type" {
+  default = "t2.micro"
+  description = "Instance type"
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+}
+
+
